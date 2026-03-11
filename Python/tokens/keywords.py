3187c409ee7cf205fa4638d7e7bf986cc546cb30
Yuvraj Gadhadi <gadhadiyuvraj@gmail.com>
2026-03-11 21:49:09 +0530

    Initial development workspace

---
+++ Python/tokens/keywords.py
@@ -0,0 +1,13 @@
+#What are Python Keywords?
+# Keywords are reserved words in Python that have a special meaning and cannot be used as variable names, function names, or identifiers.
+
+
+# Open CMD:
+
+# C:\Users\yuvra>python
+# Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import keyword
+# >>> keyword.kwlist
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+# >>>
---
+++ Python/tokens/variables.py
@@ -0,0 +1,39 @@
+# A variable in Python is a name that is used to refer to a memory location where a value is stored. Variables are used to store data that can be manipulated and referenced throughout a program.
+
+# In Python, you declare a variable by simply assigning a value to it using the assignment operator =. 
+
+# syntax:
+# variable_name = value
+
+# x = 10
+# name = "Alice"
+# price = 567.87
+
+# num1 = input("Enter a num1: ")
+
+# print(type(num1))
+
+
+# age = int(input("Enter your age: "))
+# print(age)
+# print(type(age))
+
+
+# num1, num2 = 10, 20
+# print(num1, num2)
+
+# num1 = num2 = 10
+# print(num1, num2)
+
+# name = input("Enter your name: ")
+# print(name)
+
+# input func return str type value
+
+# fname, lname = input("Enter your first and last name sep. by comma: ").split(",")
+
+# print(fname, lname)
+
+# num = 10
+# num = 20
+# print(num)
