ff980ffce529f951cc423f2d3bfa63ad56de9466
Gadhadi Yuvraj <gadhadiyuvraj@gmail.com>
2026-03-15 22:28:12 +0530

    range.py

--- Python/Loops/range.py
+++ Python/Loops/range.py
@@ -14,6 +14,10 @@
 # stop → stopping point (not included) so if you give number 11 then it will stop at 10
 # step → how much the number increases or decreases each time
 
+# ✅ Key idea:
+# Positive step (+) → numbers increase
+# Negative step (-) → numbers decrease
+
 print(list(range(11))) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list(range(1,11))) # starts from 1 and goes to 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -28,9 +32,29 @@ print(list(range(1,11,2)))
 # So it prints: 1, 3, 5, 7, 9
 
 print((list(range(1,11,3))))# Step 3 means the numbers increase by 2 each time.
-# so output is [1, 4, 7, 10] it starts from 1 ,  1+3 = 4 , 4+3 = 7, 7+3  = 10
+# so output is [1, 4, 7, 10] 
+# It starts from 1 → 1 + 3 = 4 → 4 + 3 = 7 → 7 + 3 = 10
 
-#lets learn about right to left now 
+# Now let's learn about right to left (reverse order) 
 
 print(list(range(11, 0, -1)))
+
+# Start from 11 and decrease by 1 each time because the step is -1.
+# The stop value (0) is not included.
+# So the output is: [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+print(list(range(11, 0, -2)))
+#output is : [11, 9, 7, 5, 3, 1] 
+# Step -2 means the number decreases by 2 each time.
+
+
+# ✅ Key idea:
+# Positive step (+) → numbers increase
+# Negative step (-) → numbers decrease
+
+# 11 → 11 - 2 = 9
+# 9 → 9 - 2 = 7
+# 7 → 7 - 2 = 5
+# 5 → 5 - 2 = 3
+# 3 → 3 - 2 = 1
 
