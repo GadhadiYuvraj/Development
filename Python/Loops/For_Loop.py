9e39e02c92a894e1ed69a341ff33d287eb96c83d
Gadhadi Yuvraj <gadhadiyuvraj@gmail.com>
2026-03-16 22:42:48 +0530

    For Loop

---
+++ Python/Loops/For_Loop.py
@@ -0,0 +1,29 @@
+# A for loop is used for iterating over a sequence 
+# (that is either a list, a tuple, a dictionary, a set, or a string).
+
+# This is less like the for keyword in other programming languages,
+# and works more like an iterator method as found in other object-orientated programming languages.
+
+# With the for loop we can execute a set of statements, 
+# once for each item in a list, tuple, set etc.
+
+# Syntax:
+
+# for var in iterable_obj
+#     code of block
+
+# finit loop
+
+# name ="python"
+
+# for ch in name:
+#     print(ch)
+
+
+num = 15
+
+for num in range(1, num + 1):
+    print(num)
+else:
+    print("loop Execution is completed")
+
