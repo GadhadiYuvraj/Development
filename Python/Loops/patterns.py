3e2c156a9a292b0d360d1e5645698a728d1d03f4
Gadhadi Yuvraj <gadhadiyuvraj@gmail.com>
2026-03-17 07:43:06 +0530

    Patterns in python

---
+++ Python/Loops/patterns.py
@@ -0,0 +1,48 @@
+# * * * * *   ➡️ Row
+# * * * * *   ⬇️ colum 
+# * * * * *
+# * * * * *
+# * * * * *
+
+# num = 5
+
+# for row in range(num):
+#     for col in range(num):
+#         print(" * ", end="")
+#     print()
+
+
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+
+
+# num = int(input("Enter a Number : "))
+# OR
+# num = 5
+# for row in range(num):
+#     for col in range(row +1 ):
+#         print(" * ", end=" ")
+#     print()
+
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# * 
+
+num = 5
+
+for row in range(num):
+    for col in range(row, num):
+        print(" * ", end = " ")
+    print()
+
+
+
+
+
