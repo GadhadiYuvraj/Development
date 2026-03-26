2cb53163b26e5b8e2a1d6b7fa816c9264fd50c7b
Gadhadi Yuvraj <gadhadiyuvraj@gmail.com>
2026-03-26 21:53:18 +0530

    projet 1

--- JavaScript/project1.js
+++ JavaScript/project1.js
@@ -6,8 +6,13 @@ console.log("Hello", name, "Welcome to our game!");
 
 const shouldWePlay = prompt('Do you want to play ? ')
 
-if(shouldWePlay == 'Y')
-    console.log("Lets start then");
+// if(shouldWePlay == 'Y' || shouldWePlay == 'y')
+//     console.log("Lets start then");
+/* --------- OR --------- */ 
+//  const condition = shouldWePlay == 'Y' || shouldWePlay == 'y'
+//     console.log(condition);
     
 
+const condition = shouldWePlay.toLowerCase() === "yes"
+console.log(condition);
 
